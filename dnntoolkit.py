5684b0db730b9ecfe5a120124b25721b193f4895
TrungNT <nickartin13@gmail.com>
2016-01-03 02:36:49 +0100

    refactor

--- dnntoolkit.py
+++ dnntoolkit.py
@@ -304,14 +304,14 @@ class tensor():
         return res
 
     @staticmethod
-    def to_categorical(y, nb_classes=None):
+    def to_categorical(y, n_classes=None):
         '''Convert class vector (integers from 0 to nb_classes)
         to binary class matrix, for use with categorical_crossentropy
         '''
         y = np.asarray(y, dtype='int32')
-        if not nb_classes:
-            nb_classes = np.max(y) + 1
-        Y = np.zeros((len(y), nb_classes))
+        if not n_classes:
+            n_classes = np.max(y) + 1
+        Y = np.zeros((len(y), n_classes))
         for i in range(len(y)):
             Y[i, y[i]] = 1.
         return Y
