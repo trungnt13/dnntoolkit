8475d457d5f59896f69491897a4f72117b85d676
TrungNT <nickartin13@gmail.com>
2016-02-01 13:49:05 +0200

    update

--- dnntoolkit.py
+++ dnntoolkit.py
@@ -854,7 +854,7 @@ class dnn():
         '''
         nparams *= np.sqrt(nlayers)
         # very heuristic value
-        l2value = min(10**np.log10(1. / nparams**(1/2.1)), maxval)
+        l2value = min(10**np.log10(1. / nparams**(1/2.03)), maxval)
         return np.cast[theano.config.floatX](l2value)
 
     @staticmethod
